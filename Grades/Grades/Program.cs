a3ed6c25cc2deff83aecb0140618977a65320c8c
Roman Lazunin <rlazunin@gmail.com>
2016-10-19 00:57:35 +0200

    create object GradeBook

--- Grades/Grades/Program.cs
+++ Grades/Grades/Program.cs
@@ -10,6 +10,9 @@ namespace Grades
     {
         static void Main(string[] args)
         {
+            var book = new GradeBook();
+            book.AddGrade(91);
+            book.AddGrade(89.5f);
         }
     }
 }
