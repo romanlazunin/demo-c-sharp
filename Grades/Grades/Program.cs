7a092308315ebf83e6007764b6ade36fc87d64aa
Roman Lazunin <rlazunin@gmail.com>
2016-10-19 01:05:25 +0200

    add object copy

--- Grades/Grades/Program.cs
+++ Grades/Grades/Program.cs
@@ -13,6 +13,9 @@ namespace Grades
             var book = new GradeBook();
             book.AddGrade(91);
             book.AddGrade(89.5f);
+
+            var book2 = book;
+            book2.AddGrade(75);
         }
     }
 }
